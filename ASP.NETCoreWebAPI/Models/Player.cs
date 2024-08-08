e426b3d8c5d2220f445939b5ef3f11bfdc19a6cf
Klaes Sørensen <Klaesdrescher@gmail.com>
2024-08-08 22:39:58 +0200

    push

--- ASP.NETCoreWebAPI/Models/Player.cs
+++ ASP.NETCoreWebAPI/Models/Player.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; } // Primary Key
         public required string IGN { get; set; } // Attribute - Required, In-Game Name of the player
-        public GraphicalAsset? Sprite { get; set; } // Foreign Key (GraphicalAsset) - Nullable, link or identifier for the player's sprite
+        public required GraphicalAsset Sprite { get; set; } // Foreign Key (GraphicalAsset) - Nullable, link or identifier for the player's sprite
         public Achievement[]? Achievements { get; set; } // Multi-Valued Attribute - Nullable, list of achievements associated with the player
     }
 }
