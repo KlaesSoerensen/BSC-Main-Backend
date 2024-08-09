87e8d790f75b3fcb99d96f5957b6e877cc79f4e2
Klaes Sørensen <Klaesdrescher@gmail.com>
2024-08-09 13:30:26 +0200

    ResponseDTOs

---
+++ BSC Main Backend/dto/GetAssetLODResponseDTO.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection.Metadata;
+
+namespace BSC_Main_Backend.dto;
+
+public record GetAssetLODResponseDTO(
+        uint Id,
+        uint DetailLevel,
+        Blob Blob // Maybe uint8[]
+    );
---
+++ BSC Main Backend/dto/MiniGameInfoResposneDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace BSC_Main_Backend.dto;
+
+public record MiniGameInfoResposneDTO(
+        uint Id,
+        string Name,
+        uint Icon,
+        string Description,
+        Object Difficualties //TODO: What Type here
+    );
