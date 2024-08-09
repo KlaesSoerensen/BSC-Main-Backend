2fe9556cd2c7f282ef5e9ba369df7acba0776005
Klaes Sørensen <Klaesdrescher@gmail.com>
2024-08-09 12:35:39 +0200

    Redo

--- ASP.NETCoreWebAPI/Models/ColonyAsset.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ASP.NETCoreWebAPI.Models
-{
-    public class ColonyAsset
-    {
-        public int Id { get; set; } // Primary Key
-        public AssetCollection AssetCollection { get; set; } // Foreign Key (AssetCollection)
-        public Transform Transform { get; set; } // Foreign Key (Transform)
-    }
-}
