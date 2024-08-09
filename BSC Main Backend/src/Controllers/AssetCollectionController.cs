2bc2dcab9996f66d690137339754a351821463e3
Klaes Sørensen <Klaesdrescher@gmail.com>
2024-08-09 14:33:35 +0200

    import issue

--- BSC Main Backend/src/Controllers/AssetCollectionController.cs
+++ BSC Main Backend/src/Controllers/AssetCollectionController.cs
@@ -1,6 +1,40 @@
-﻿namespace BSC_Main_Backend.Controllers;
+﻿using BSC_Main_Backend.dto.response;
+using BSC_Main_Backend.dto;
+using Microsoft.AspNetCore.Mvc;
 
-public class AssetCollectionController
+namespace BSC_Main_Backend.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class AssetCollectionController : ControllerBase
 {
+    
+    private readonly ILogger<AssetCollectionController> _logger;
+
+    public AssetCollectionController(ILogger<AssetCollectionController> logger)
+    {
+        _logger = logger;
+    }
+    
+    [HttpGet(Name = "<base-URL>/collection/<collectionId>")]
+    public FetchAssetCollectionResponseDTO GetAssetCollection()
+    {
+        // Fetch from database and perform logic as needed.
+        var entries = new List<AssetCollectionEntryDTO>
+        {
+            // new AssetCollectionEntryDTO(1, new TransformDTO()),
+            // new AssetCollectionEntryDTO(2, new TransformDTO())
+        };
+
+        var response = new FetchAssetCollectionResponseDTO(
+            Id: 123,
+            Original: 456,
+            Name: "Sample Collection",
+            Entries: entries
+        );
+
+        return null; // response
+    }
     
 }
--- BSC Main Backend/src/Controllers/AssetController.cs
+++ BSC Main Backend/src/Controllers/AssetController.cs
@@ -1,6 +1,29 @@
-﻿namespace BSC_Main_Backend.Controllers;
+﻿using BSC_Main_Backend.dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BSC_Main_Backend.Controllers;
+
+[ApiController]
+[Route("[controller]")]
 
 public class AssetController
 {
+    private readonly ILogger<AssetController> _logger;
+
+    public AssetController(ILogger<AssetController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "<base-URL>/asset/<assetId>")]
+    public FetchAssetResponseDTO GetAsset()
+    {
+        return null;
+    }
     
+    [HttpGet(Name = "<base-URL>/assets?ids=x,y,z")]
+    public FetchAssetResponseDTO GetAssets()
+    {
+        return null;
+    }
 }
