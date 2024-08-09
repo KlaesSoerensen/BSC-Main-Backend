aaf97ee584c56c20ca306aef912707fa65353069
Klaes Sørensen <Klaesdrescher@gmail.com>
2024-08-09 14:33:35 +0200

    import issue

--- BSC Main Backend/src/Controllers/ColonyController.cs
+++ BSC Main Backend/src/Controllers/ColonyController.cs
@@ -1,6 +1,28 @@
-﻿namespace BSC_Main_Backend.Controllers;
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BSC_Main_Backend.Controllers;
+
+[ApiController]
+[Route("[controller]")]
 
 public class ColonyController
 {
+    private readonly ILogger<ColonyController> _logger;
+
+    public ColonyController(ILogger<ColonyController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "<base-URL>/asset/<assetId>")]
+    public FetchAssetResponseDTO GetAsset()
+    {
+        return null;
+    }
     
+    [HttpGet(Name = "<base-URL>/assets?ids=x,y,z")]
+    public FetchAssetResponseDTO GetAssets()
+    {
+        return null;
+    }
 }
