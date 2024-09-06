cd1ca3558f7123b1384641036b29f60e61451750
lilybw <lilybwanscher@gmail.com>
2024-09-06 21:09:24 +0200

    Catalogue api to spec
    Also resolved a logging bug left over from changing error handling on auth checks

--- src/api/catalogApi.go
+++ src/api/catalogApi.go
@@ -21,6 +21,11 @@ func applyCatalog(app *fiber.App, appContext *meta.ApplicationContext) error {
 	return nil
 }
 
+type CatalogueEntry struct {
+	Key   string
+	Value string
+}
+
 func getCatalogueForLanguageHandler(c *fiber.Ctx, appContext *meta.ApplicationContext) error {
 	language := c.Params("language")
 	keysStr := strings.Trim(c.Query("keys"), " ")
@@ -35,60 +40,67 @@ func getCatalogueForLanguageHandler(c *fiber.Ctx, appContext *meta.ApplicationCo
 		c.Response().Header.Set(appContext.DDH, "Language path parameter missing")
 		return fiber.NewError(fiber.StatusBadRequest, "Language path parameter missing")
 	}
-	var data map[string]string = make(map[string]string)
+	var data []CatalogueEntry
 	var dbErr *fiber.Error
 	if isUsingKeys {
-		if data, dbErr = getCatalogueForSpecificKeys(language, keysSplit, appContext, c); dbErr != nil {
+		if dbErr = getCatalogueForSpecificKeys(language, keysSplit, appContext, c, &data); dbErr != nil {
 			return dbErr
 		}
 	} else {
-		if data, dbErr = getFullCatalogueForLanguage(language, appContext, c); dbErr != nil {
+		if dbErr = getFullCatalogueForLanguage(language, appContext, c, &data); dbErr != nil {
 			return dbErr
 		}
 	}
 	//Statuscode set by getFullCatalogueForLanguage or getCatalogueForSpecificKeys
-	return c.JSON(data)
+	// Convert the results into a map
+	json := make(map[string]string)
+	for _, result := range data {
+		json[result.Key] = result.Value
+	}
+
+	c.Status(fiber.StatusOK)
+	return c.JSON(json)
 }
 
 // Also sets headers and status code on ctx if present
-func getFullCatalogueForLanguage(language string, appContext *meta.ApplicationContext, ctx *fiber.Ctx) (InternationalizationCatalogue, *fiber.Error) {
-	var data map[string]string
+func getFullCatalogueForLanguage(language string, appContext *meta.ApplicationContext, ctx *fiber.Ctx, dest *[]CatalogueEntry) *fiber.Error {
 	log.Printf("[delete me] Executing query for language: %s", language)
 	if dbErr := appContext.LanguageDB.
 		Table("Catalogue").
-		Select("key, \"" + language + "\"").
-		Scan(&data).Error; dbErr != nil {
+		//This alias is needed as GORM matches on the struct field name, which is dynamic in this case
+		Select("key, \"" + language + "\" AS value").
+		Scan(dest).Error; dbErr != nil {
 
 		if errors.Is(dbErr, gorm.ErrRecordNotFound) {
 			ctx.Response().Header.Set(appContext.DDH, "No such language catalogue found")
-			return nil, fiber.NewError(fiber.StatusNotFound, "No such language catalogue found")
+			return fiber.NewError(fiber.StatusNotFound, "No such language catalogue found")
 		}
 
 		ctx.Response().Header.Set(appContext.DDH, "Internal error "+dbErr.Error())
-		return nil, fiber.NewError(fiber.StatusInternalServerError, "Internal error "+dbErr.Error())
+		return fiber.NewError(fiber.StatusInternalServerError, "Internal error "+dbErr.Error())
 	}
 	ctx.Status(fiber.StatusOK)
-	return data, nil
+	return nil
 }
 
 // Also sets headers and status code on ctx if present
-func getCatalogueForSpecificKeys(language string, keys []string, appContext *meta.ApplicationContext, ctx *fiber.Ctx) (InternationalizationCatalogue, *fiber.Error) {
-	var data map[string]string
-	log.Printf("[deleteme] Executing query for language: %s, keys: %v", language, keys)
+func getCatalogueForSpecificKeys(language string, keys []string, appContext *meta.ApplicationContext, ctx *fiber.Ctx, dest *[]CatalogueEntry) *fiber.Error {
+	log.Printf("[delete me] Executing query for language: %s, keys: %v", language, keys)
 	if dbErr := appContext.LanguageDB.
 		Table("Catalogue").
-		Select("key, \""+language+"\"").
+		//This alias is needed as GORM matches on the struct field name, which is dynamic in this case
+		Select("key, \""+language+"\" AS value").
 		Where("key IN ?", keys).
-		Scan(&data).Error; dbErr != nil {
+		Scan(dest).Error; dbErr != nil {
 
 		if errors.Is(dbErr, gorm.ErrRecordNotFound) {
 			ctx.Response().Header.Set(appContext.DDH, "No such language catalogue found")
-			return nil, fiber.NewError(fiber.StatusNotFound, "No such language catalogue found")
+			return fiber.NewError(fiber.StatusNotFound, "No such language catalogue found")
 		}
 
 		ctx.Response().Header.Set(appContext.DDH, "Internal error "+dbErr.Error())
-		return nil, fiber.NewError(fiber.StatusInternalServerError, "Internal error "+dbErr.Error())
+		return fiber.NewError(fiber.StatusInternalServerError, "Internal error "+dbErr.Error())
 	}
 	ctx.Status(fiber.StatusOK)
-	return data, nil
+	return nil
 }
