65e1676c7ae2b7d3aa311f6f5112d2f118b9559d
lilybw <lilybwanscher@gmail.com>
2024-09-06 16:29:19 +0200

    New auth solution
    Now logs correctly too! Although only routes with auth gets logged...

---
+++ src/auth/auth.go
@@ -0,0 +1,39 @@
+package auth
+
+import (
+	"fmt"
+	"otte_main_backend/src/meta"
+	"otte_main_backend/src/middleware"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+var errorUnauthorized error = fmt.Errorf("Unauthorized")
+
+// Prefixes an authcheck function onto the original handler function
+//
+// if the auth check fails, it assures the handler isn't run
+//
+// Also sets debug header and status code on auth error
+func PrefixOn(appContext *meta.ApplicationContext, existingHandler func(c *fiber.Ctx, appContext *meta.ApplicationContext) error) func(*fiber.Ctx) error {
+	return func(c *fiber.Ctx) error {
+		if err := naiveCheckForHeaderAuth(c, appContext.AuthTokenName, appContext.DDH); err != nil {
+			middleware.LogRequests(c)
+			return nil
+		}
+		handlerErr := existingHandler(c, appContext)
+		middleware.LogRequests(c)
+		return handlerErr
+	}
+}
+
+func naiveCheckForHeaderAuth(context *fiber.Ctx, tokenName string, defaultDebugHeader string) error {
+	authHeaderContent := context.Request().Header.Peek(tokenName)
+	if len(authHeaderContent) == 0 {
+		context.Response().Header.Set(defaultDebugHeader, "Missing auth header, expected "+tokenName+" to be present")
+		context.Status(401)
+		return errorUnauthorized
+	}
+
+	return nil
+}
