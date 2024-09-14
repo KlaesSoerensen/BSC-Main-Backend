e284a414580ee309afa1d54ba212527d46772b87
GustavBW <gustavbw@gmail.com>
2024-09-14 11:17:40 +0200

    Starting on Session API

--- src/api/sessionApi.go
+++ src/api/sessionApi.go
@@ -3,6 +3,7 @@ package api
 import (
 	"log"
 	"otte_main_backend/src/meta"
+	"otte_main_backend/src/middleware"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -10,5 +11,15 @@ import (
 func applySessionApi(app *fiber.App, appContext *meta.ApplicationContext) error {
 	log.Println("[Session API] Applying session API")
 
+	//No Auth required
+	app.Post("/api/v1/session", func(c *fiber.Ctx) error { return initiateSessionHandler(c, appContext) })
+
 	return nil
+}
+
+func initiateSessionHandler(c *fiber.Ctx, appContext *meta.ApplicationContext) error {
+
+	c.Status(fiber.StatusInternalServerError)
+	middleware.LogRequests(c)
+	return fiber.NewError(fiber.StatusNotImplemented, "Not implemented")
 }
