490d45605f80cf5cef76e56d5f6831f8721d9817
GustavBW <gustavbw@gmail.com>
2024-09-14 12:57:52 +0200

    Vitec Integration

--- src/api/sessionApi.go
+++ src/api/sessionApi.go
@@ -1,17 +1,15 @@
 package api
 
 import (
+	"errors"
 	"log"
 	"otte_main_backend/src/meta"
 	"otte_main_backend/src/middleware"
+	"otte_main_backend/src/vitec"
 
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
 )
-
-type SessionInitiationDTO struct {
-	UserIdentifier      string `json:"userIdentifier"`
-	CurrentSessionToken string `json:"currentSessionToken"`
-}
 
 func applySessionApi(app *fiber.App, appContext *meta.ApplicationContext) error {
 	log.Println("[Session API] Applying session API")
@@ -23,7 +21,7 @@ func applySessionApi(app *fiber.App, appContext *meta.ApplicationContext) error 
 }
 
 func initiateSessionHandler(c *fiber.Ctx, appContext *meta.ApplicationContext) error {
-	var body SessionInitiationDTO
+	var body vitec.SessionInitiationDTO
 	//Extract request body
 	if err := c.BodyParser(&body); err != nil {
 		return fiber.NewError(fiber.StatusBadRequest, "Invalid request body")
@@ -31,7 +29,16 @@ func initiateSessionHandler(c *fiber.Ctx, appContext *meta.ApplicationContext) e
 	var player PlayerDTO
 	//Check if player exists in PlayerDB - if so, all is well
 	if err := appContext.PlayerDB.Where("id = ?", body.UserIdentifier).First(&player).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			//Expected outcome.
 
+			//Check with vitec
+
+		} else {
+			c.Status(fiber.StatusUnauthorized)
+			middleware.LogRequests(c)
+			return fiber.NewError(fiber.StatusUnauthorized, "Unable to cross-verify user")
+		}
 	}
 
 	//If not, check with Vitec
