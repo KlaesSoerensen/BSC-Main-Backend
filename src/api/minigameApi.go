44df15d59058d399b1607a942653465b701b9da7
lilybw <lilybwanscher@gmail.com>
2024-09-14 10:50:19 +0200

    Minimized Minigame Info

--- src/api/minigameApi.go
+++ src/api/minigameApi.go
@@ -19,7 +19,7 @@ type MinimizedMinigameDTO struct {
 type MinigameDifficultyDTO struct {
 	ID                  uint32 `json:"id"`
 	Name                string `json:"name"`
-	MinigameID          uint32 `json:"minigameID" gorm:"column:minigame"`
+	MinigameID          uint32 `json:"-" gorm:"column:minigame"`
 	Icon                uint32 `json:"icon"`
 	Description         string `json:"description"`
 	OverwritingSettings string `json:"overwritingSettings" gorm:"column:overwritingSettings"`
@@ -46,8 +46,44 @@ func applyMinigameApi(app *fiber.App, appContext *meta.ApplicationContext) error
 	log.Println("[Minigame API] Applying Minigame API")
 
 	app.Get("/api/v1/minigames/:id", auth.PrefixOn(appContext, getMinigameInfoHandler))
+	app.Get("/api/v1/minigames/minimized", auth.PrefixOn(appContext, getMinimizedMinigameHandler))
 
 	return nil
+}
+
+func getMinimizedMinigameHandler(c *fiber.Ctx, appContext *meta.ApplicationContext) error {
+	idStr := c.Query("id")
+	diffStr := c.Query("difficulty")
+	minigameID, idParseErr := strconv.Atoi(idStr)
+	if idParseErr != nil {
+		c.Response().Header.Set(appContext.DDH, "Error in parsing minigame id "+idParseErr.Error())
+		return fiber.NewError(fiber.StatusBadRequest, "Error in parsing minigame id "+idParseErr.Error())
+	}
+	diffID, diffParseErr := strconv.Atoi(diffStr)
+	if diffParseErr != nil {
+		c.Response().Header.Set(appContext.DDH, "Error in parsing minigame difficulty "+diffParseErr.Error())
+		return fiber.NewError(fiber.StatusBadRequest, "Error in parsing minigame difficulty "+diffParseErr.Error())
+	}
+
+	var minigame MinimizedMinigameDTO
+	if err := appContext.ColonyAssetDB.
+		Table("MiniGame").
+		Select(`"Minigame".settings", "MiniGameDifficulty"."overwritingSettings"`).
+		Where(`"MiniGame".id = ?`, minigameID).
+		Joins("JOIN MiniGameDifficulty ON MiniGameDifficulty.minigame = MiniGame.id AND MiniGameDifficulty.id = ?", diffID).
+		First(&minigame).Error; err != nil {
+
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			c.Response().Header.Set(appContext.DDH, "No such minigame or minigame difficulty")
+			return fiber.NewError(fiber.StatusNotFound, "No such minigame or minigame difficulty")
+		}
+
+		c.Response().Header.Set(appContext.DDH, "Error in fetching minigame "+err.Error())
+		return fiber.NewError(fiber.StatusInternalServerError, "Error in fetching minigame "+err.Error())
+	}
+
+	c.Status(fiber.StatusOK)
+	return c.JSON(minigame)
 }
 
 func getMinigameInfoHandler(c *fiber.Ctx, appContext *meta.ApplicationContext) error {
@@ -64,7 +100,7 @@ func getMinigameInfoHandler(c *fiber.Ctx, appContext *meta.ApplicationContext) e
 		Table("MiniGame").
 		Preload("Difficulties").
 		Where(`"MiniGame".id = ?`, id).
-		Find(&minigame).Error; err != nil {
+		First(&minigame).Error; err != nil {
 
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			c.Response().Header.Set(appContext.DDH, "No such minigame")
