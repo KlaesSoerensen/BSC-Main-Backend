8a5d7a04f9a8e9d392c769d97a1cc4de1aafca6d
GustavBW <gustavbw@gmail.com>
2024-10-05 15:57:54 +0200

    Multiplayer Backend Healthcheck

--- src/multiplayer/integration.go
+++ src/multiplayer/integration.go
@@ -5,10 +5,16 @@ import (
 	"fmt"
 	"net/http"
 	"otte_main_backend/src/meta"
+	"time"
 )
 
 type CreateLobbyResponseDTO struct {
 	ID uint32 `json:"id"`
+}
+
+type HealthCheckResponseDTO struct {
+	Status     bool   `json:"status"`
+	LobbyCount uint32 `json:"lobbyCount"`
 }
 
 // Returns lobbyID, error
@@ -21,6 +27,39 @@ func CreateLobby(ownerID uint32, colonyID uint32, appContext *meta.ApplicationCo
 	return body.ID, nil
 }
 
+func CheckConnection(appContext *meta.ApplicationContext) (*HealthCheckResponseDTO, error) {
+	url := fmt.Sprintf("%s/health", appContext.MultiplayerServerAddress)
+	resp, err := makeGetRequest[HealthCheckResponseDTO](url)
+	if err != nil {
+		return nil, fmt.Errorf("error checking connection: %v", err)
+	}
+	return resp, nil
+}
+
+func makeGetRequest[T any](url string) (*T, error) {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("error performing multiplayer backend healthcheck: %v", err)
+	}
+	client := &http.Client{Timeout: 10 * time.Second}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error performing multiplayer backend healthcheck: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected status code from multiplayer backend: %d", resp.StatusCode)
+	}
+	var dest T
+	err = json.NewDecoder(resp.Body).Decode(dest)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing response body: %v", err)
+	}
+
+	return &dest, nil
+}
+
 func makeEmptyPostRequest(url string) (*CreateLobbyResponseDTO, error) {
 	// Create a new POST request with no body
 	req, err := http.NewRequest("POST", url, nil)
@@ -29,7 +68,7 @@ func makeEmptyPostRequest(url string) (*CreateLobbyResponseDTO, error) {
 	}
 
 	// Send the request
-	client := &http.Client{}
+	client := &http.Client{Timeout: 10 * time.Second}
 	resp, err := client.Do(req)
 	if err != nil {
 		return nil, fmt.Errorf("error sending request: %v", err)
