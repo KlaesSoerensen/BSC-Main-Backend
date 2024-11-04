ba03e20d0ba8121017cd3f6d9f5412dc2b2387a6
Klaes Sørensen <Klaesdrescher@gmail.com>
2024-11-04 13:56:54 +0100

    Update transforms.go

--- src/api/colony/transforms.go
+++ src/api/colony/transforms.go
@@ -33,17 +33,17 @@ func createTransform(xScale float64, yScale float64, xOffset float64, yOffset fl
 
 func InsertTransforms(appContext *meta.ApplicationContext, tx *gorm.DB) (map[string]uint, error) {
 	transforms := []Transform{
-		createTransform(1, 1, 400, 400, 100),  // Town Hall
-		createTransform(1, 1, 250, 300, 100),  // Cantina
-		createTransform(1, 1, 100, 400, 100),  // Home
-		createTransform(1, 1, 400, 200, 100),  // Aquifer Plant
-		createTransform(1, 1, 400, 600, 100),  // Shield Generator
-		createTransform(1, 1, 700, 400, 100),  // Vehicle Storage
-		createTransform(1, 1, 1000, 300, 100), // Radar Dish
-		createTransform(1, 1, 1000, 500, 100), // Mining Facility
-		createTransform(1, 1, 1300, 300, 100), // Outer Walls
-		createTransform(1, 1, 1300, 500, 100), // Space Port
-		createTransform(1, 1, 400, 0, 100),    // Agriculture Center
+		createTransform(1, 1, 650, 400, 100),  // Town Hall
+		createTransform(1, 1, 400, 280, 100),  // Cantina
+		createTransform(1, 1, 220, 380, 100),  // Home
+		createTransform(1, 1, 850, 220, 100),  // Aquifer Plant
+		createTransform(1, 1, 600, 580, 100),  // Shield Generator
+		createTransform(1, 1, 1020, 420, 100), // Vehicle Storage
+		createTransform(1, 1, 1450, 300, 100), // Radar Dish
+		createTransform(1, 1, 1400, 500, 100), // Mining Facility
+		createTransform(1, 1, 1750, 280, 100), // Outer Walls
+		createTransform(1, 1, 1800, 450, 100), // Space Port
+		createTransform(1, 1, 620, 150, 100),  // Agriculture Center
 	}
 
 	transformIDs := make(map[string]uint)
